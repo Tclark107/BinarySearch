97c513983311985642115344615e6dbdb73bdca6
trclark <trclark@ucsc.edu>
2019-09-07 17:58:03 -0700

    All the set up should be done now

---
+++ BinarySearch.cpp
@@ -0,0 +1,17 @@
+//-----------------------------------------------------------------------------------------
+// BinarySearch.cpp
+//
+// My first attempt at using Github during the process of a project.  
+// Here is my version of a binary search algorithm in C++.  
+// It will be tested on the command line.
+//
+// Tristan Clark
+// 9/7/19
+//-----------------------------------------------------------------------------------------
+
+#include<iostream>
+using namespace std;
+
+int main() {
+	cout << "Text" << endl;
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+----------------------------------------------------------------------
+BinarySearch
+----------------------------------------------------------------------
+This will be my first attempt keeping a project up to date with Git.  
+I am practicing with a binary search algorithm.
+The language I am using is C++.
+----------------------------------------------------------------------
